4754acbb541d448f70325cf1f81b773c5351dc34
Yet-Zio <maheshdeluxe53@gmail.com>
2020-06-02 20:51:15 +0530

    Update setup.py

--- Python/0.5.2/setup.py
+++ Python/0.5.2/setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'PySoul',
   packages = ['PySoul'],
-  version = '0.5.2',
+  version = '0.5.3',
   license='CC0 1.0 Universal',
   description = 'Python Implementation for handling Soul Configuration files',
   author = 'Yet-Zio',
   author_email = 'maheshdeluxe53@gmail.com',
   url = 'https://text-x-soul.tk',
-  download_url = 'https://github.com/text-x-soul/text-x-soul/raw/master/Python/0.5.2/releases/0.5.2/PySoul-0.5.2.tar.gz',
+  download_url = 'https://github.com/text-x-soul/text-x-soul/raw/master/Python/0.5.2/releases/0.5.3/PySoul-0.5.3.tar.gz',
   keywords = ['PUBLIC', 'SERIALIZATION', 'CONFIGURATION'],
   classifiers=[
     'Development Status :: 5 - Production/Stable',
