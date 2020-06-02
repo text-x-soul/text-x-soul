b78ce53d746a1cb4c765f637b792e5fab5606c54
Yet-Zio <maheshdeluxe53@gmail.com>
2020-06-02 21:34:06 +0530

    Update setup.py

--- Python/0.5.2/setup.py
+++ Python/0.5.2/setup.py
@@ -1,14 +1,22 @@
 from distutils.core import setup
+
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
   name = 'PySoul',
+  long_description=long_description,
+  long_description_content_type='text/x-rst',
   packages = ['PySoul'],
-  version = '0.5.3',
+  version = '0.5.4',
   license='CC0 1.0 Universal',
   description = 'Python Implementation for handling Soul Configuration files',
   author = 'Yet-Zio',
   author_email = 'maheshdeluxe53@gmail.com',
   url = 'https://text-x-soul.tk',
-  download_url = 'https://github.com/text-x-soul/text-x-soul/raw/master/Python/0.5.2/releases/0.5.3/PySoul-0.5.3.tar.gz',
+  download_url = 'https://github.com/text-x-soul/text-x-soul/raw/master/Python/0.5.2/releases/0.5.4/PySoul-0.5.4.tar.gz',
   keywords = ['PUBLIC', 'SERIALIZATION', 'CONFIGURATION'],
   classifiers=[
     'Development Status :: 5 - Production/Stable',
